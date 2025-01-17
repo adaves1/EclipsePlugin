29177dbaaccf087ac31adab53021e05101c83b4e
Anthony Daves <adaves1@yahoo.com>
2025-01-17 21:50:06 +0530

    Create Main.java

---
+++ src/com/Main.java
@@ -0,0 +1,11 @@
+package com.adaves1;
+
+// Import the EclipsePluginLoader class
+import com.adaves1.EclipsePluginLoader;
+
+public class Main {
+    public static void main(String[] args) {
+        // Call the main method of EclipsePluginLoader
+        EclipsePluginLoader.main(args);
+    }
+}
